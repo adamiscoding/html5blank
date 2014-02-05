8361aeccdbfbeaa18793d581c0c84f447d4a331d
Adam Huchla <ahuchla@gmail.com>
2014-02-05 21:46:08 +0100

    added source maps to sass grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'compressed'
+                    style: 'compressed',
+                    sourcemap: true
                 },
                 files: {
                     'css/main.css': 'css/main.scss'
